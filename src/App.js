5965738dca71c0ed0f27f23a46e51035df4f707e
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-08-12 11:03:13 -0300

    Add About us for phone and desktop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from './components/Landing/About';
 import Hero from './components/Landing/Hero';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <About />
     </div>
   );
 }
