80c6910390324e440810d45620b05b44041fbfd2
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-08-15 20:02:21 -0300

    Add jobs and education render to Cv component

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 
 export class Cv extends Component {
   render() {
-    const { personal, extra } = this.props.state;
+    const { personal, extra, jobs, education } = this.props.state;
+    const jobsList = jobs.map((job) => {
+      return (
+        <div>
+          <p>{job.place}</p>
+          <p>{job.rol}</p>
+          <p>{job.description}</p>
+          <p>{job.datestart}</p>
+          <p>{job.dateend}</p>
+          <p>{job.city}</p>
+          <p>{job.country}</p>
+        </div>
+      );
+    });
+
+    const edList = education.map((ed) => {
+      return (
+        <div>
+          <p>{ed.college}</p>
+          <p>{ed.career}</p>
+          <p>{ed.yearstart}</p>
+          <p>{ed.yearend}</p>
+        </div>
+      );
+    });
     return (
       <div className="p-4">
         <div>
@@ -18,6 +42,14 @@ export class Cv extends Component {
           <p className="font-bold">Extra</p>
           <div>{extra.personal || 'about me'}</div>
           <div>{extra.skills || 'skills'}</div>
+        </div>
+        <div>
+          <p className="font-bold">Jobs</p>
+          {jobsList}
+        </div>
+        <div>
+          <p className="font-bold">Education</p>
+          {edList}
         </div>
       </div>
     );
