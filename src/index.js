7d45bec922f98a1abbda2f2ab689010108265799
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-08-26 12:01:43 -0300

    Swap from Browser to Hash router, allowing render of form by itself

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Form from './components/Form';
 import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/cv-application">
+    <HashRouter>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/form" element={<Form />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
