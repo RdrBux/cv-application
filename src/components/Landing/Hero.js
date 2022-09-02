d61c6f34c4b285396e5feb42d56a88bd14ecc3d6
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-09-02 00:20:06 -0300

    Add arrow to buttons on landing page

--- src/components/Landing/Hero.js
+++ src/components/Landing/Hero.js
@@ -46,10 +46,24 @@ export class Hero extends Component {
               <div className="flex gap-3">
                 <Link to="/form">
                   <button
-                    className="px-10 py-3 bg-indigo-700 font-bold text-sm lg:text-base text-white rounded-lg shadow hover:bg-indigo-900 duration-200"
+                    className="flex items-center gap-1 px-10 py-3 bg-indigo-700 font-bold text-sm lg:text-base text-white rounded-lg shadow hover:bg-indigo-900 duration-200"
                     onClick={onClick}
                   >
-                    Crea tu CV
+                    Crea tu CV{' '}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-4 h-4 stroke-[3]"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                      />
+                    </svg>
                   </button>
                 </Link>
                 {/* <a href="#newsletter">
--- src/components/Landing/Nav.js
+++ src/components/Landing/Nav.js
@@ -13,10 +13,24 @@ export class Nav extends Component {
             </div>
             <Link to="/form">
               <button
-                className="bg-slate-900 hover:bg-slate-700 duration-200 text-slate-100 px-4 py-2 font-bold rounded-lg text-sm"
+                className="flex items-center gap-1 bg-slate-900 hover:bg-slate-700 duration-200 text-slate-100 px-4 py-2 font-bold rounded-lg text-sm"
                 onClick={onClick}
               >
-                Creador de CV
+                Crea tu CV{' '}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-4 h-4 stroke-[3]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                  />
+                </svg>
               </button>
             </Link>
           </div>
